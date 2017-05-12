3c629d3d9f91d10ca4f156591d830c631d7ddc85
ruffm <mvr5567@psu.edu>
2017-05-12 00:57:46 -0400

    Rework cd.py

--- cd.py
+++ cd.py
@@ -1,14 +1,25 @@
 import os
 import sys
+import platform
 
 dir = sys.argv[1]
 
-if os.path.isdir(dir):
-    pwd = os.getcwd()
-    # dir = pwd + "\\" + dir
-    absolutePath = os.path.abspath(pwd + "\\" + dir)
-    # print absolutePath
-    cd = os.chdir(absolutePath)
+if os.name == 'posix':
+   if os.path.isdir(dir):
+       pwd = os.cwd()
+       absolutePath = os.path.abspath(pwd + "/" + dir)
+   else:
+       print("Not a directory.")
+
+elif platform.system() == 'Windows':
+    if os.path.isdir(dir):
+        pwd = os.getcwd()
+        # dir = pwd + "\\" + dir
+        absolutePath = os.path.abspath(pwd + "\\" + dir)
+        # print absolutePath # test print of absolutePath
+        cd = os.chdir(absolutePath)
+    else:
+        print("Not a directory.")
 
 else:
-    print("Not a directory.")
+    print("What operating system are you on?")
